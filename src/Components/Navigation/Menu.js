f4b173ff17e27ab72234ab221d336f65324452d9
wojciech.cwiklik <wojciech.cwiklik@zondaglobal.com>
2022-04-21 20:04:43 +0200

    new menu and react route

--- src/Components/Navigation/Menu.js
+++ src/Components/Navigation/Menu.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 import { ReactComponent as YourSvg } from '../../assets/logo-light.svg';
 import './Menu.css';
 function Menu() {
   return (
-    <div>
-        {/*--==================== HEADER ====================--> */}
-        <ul>
-            <li><a href="#home" className="nav__logo" id="nav__logo"><YourSvg/></a></li>
-            <li><a  href="#home">Home</a></li>
-            <li><a href="#news">Procentage</a></li>
-            <li><a href="#contact">Spread</a></li>
-            <li><a href="#about">About</a></li>
-        </ul>
-        
-    </div>
-    
+    <>        
+    {/*--==================== HEADER ====================--> */}
+      <div className="nav">
+        <div className="left">
+        <Link to="/home"><YourSvg/></Link>
+        </div>
+        <div claclassNamess="middle">
+           <Link to="/home" className="item">Home</Link>
+           <Link to="/markets" className="item">Markets</Link>
+           <Link to="/capitalization" className="item">Capitalization</Link>
+           <Link to="/about" className="item">About</Link>
+        </div>
+        <div className="right">
+          <button class="material-symbols-outlined notification" >notifications</button> 
+          <button class="material-symbols-outlined hamburger">menu</button>
+        </div>
+      </div>
+      
+    </>
   )
 }
 
