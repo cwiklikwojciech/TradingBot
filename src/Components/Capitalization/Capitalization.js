19189161c85bdad482e797dee0a6ab16bb245885
wojciech.cwiklik <wojciech.cwiklik@zondaglobal.com>
2022-04-21 20:05:17 +0200

    New Components

---
+++ src/Components/Capitalization/Capitalization.js
@@ -0,0 +1,11 @@
+import './Capitalization.css'
+const Capitalization = () => {
+    return (
+      <>
+        <center><h1>Capitalization Page</h1></center>
+      </>
+    );
+  };
+  
+  export default Capitalization;
+  
